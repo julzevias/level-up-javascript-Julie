67305e8dc8a20f0bdab19c1a15868c26ab6ec079
Julie Yue <47709222+julzevias@users.noreply.github.com>
2023-09-27 03:05:24 +0000

    movie function and class

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,41 @@
 // Write your code here
+
+class Movie {
+
+  constructor(title, director, genre, releaseYear, rating)  {
+      this.title = title;
+      this.director = director;
+      this.genre = genre;
+      this.releaseYear = releaseYear;
+      this.rating = rating;
+  }
+
+  get getOverview() {
+    return `${this.title}, a ${this.genre} film directed by ${this.directordirector} was released in ${this.releaseYear}. It received a rating of ${this.rating}.`
+  }
+}
+
+const movie1 = new Movie("Harry Potter", "Movie Director", "fantasy", "2010", "5 star");
+
+console.log(movie1.title);
+console.log(movie1.getOverview);
+
+
+
+// function prototype
+
+function Movie2(title, director, genre, releaseYear, rating) {
+  this.title = title;
+  this.director = director;
+  this.genre = genre;
+  this.releaseYear = releaseYear;
+  this.rating = rating;
+}
+
+Movie2.prototype.getOverview = function() {
+  return `${this.title}, a ${this.genre} film directed by ${this.directordirector} was released in ${this.releaseYear}. It received a rating of ${this.rating}.`;
+}
+
+const getMovie = new Movie2('t', 'd', 'g', 'r', 'r');
+
+console.log(getMovie.getOverview());
